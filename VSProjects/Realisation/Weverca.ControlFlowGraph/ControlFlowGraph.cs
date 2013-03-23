91af267a3f65fa2fef7c01b73f5507c1d73413e2
brenner <brenner@mycompany.com>
2013-03-23 15:03:32 +0000

    vytvoření projektu pro ControlFlowGraph a zahrnutí do "globální" SLN pro wevercu (bude třeba přidat spustitelný projekt)
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@13 67bba403-d36b-46c0-a350-761a85a4c57d

---
+++ VSProjects/Realisation/Weverca.ControlFlowGraph/ControlFlowGraph.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Weverca.ControlFlowGraph
+{
+    public class ControlFlowGraph
+    {
+    }
+}
