ab4fcc8e09a5429fb8c6c1564d8c2eff111cc1fa
kikta <kikta@mycompany.com>
2013-03-23 15:21:20 +0000

    vytvorenie tried BasicBlock a BasicBlockEdge
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@14 67bba403-d36b-46c0-a350-761a85a4c57d

--- VSProjects/Realisation/Weverca.ControlFlowGraph/ControlFlowGraph.cs
+++ VSProjects/Realisation/Weverca.ControlFlowGraph/ControlFlowGraph.cs
@@ -2,10 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using PHP.Core;
+using PHP.Core.AST;
+using PHP.Core.Parsers;
+
 
 namespace Weverca.ControlFlowGraph
 {
     public class ControlFlowGraph
     {
+
+
     }
+
+    public class BasicBlock {
+        public List<Statement> Statements;
+        public List<BasicBlockEdge> OutgoingEdges;
+        public List<BasicBlockEdge> IncommingEdges;
+
+        public BasicBlock() {
+            Statements = new List<Statement>();
+            OutgoingEdges = new List<BasicBlockEdge>();
+            IncommingEdges = new List<BasicBlockEdge>();
+        }
+
+    }
+
+    public class BasicBlockEdge {
+        public BasicBlock From { set; get; }
+        public BasicBlock To { set; get; }
+        public ExpressionStmt Condition { set; get; }
+        BasicBlockEdge(BasicBlock From, BasicBlock To, ExpressionStmt Condition)
+        {
+            this.From = From;
+            this.To = To;
+            this.Condition = Condition;
+        } 
+    }
+
 }
