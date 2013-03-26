9c20f1516ea0d4266f88b151cd17f1fff0afb4dc
vodolan <vodolan@mycompany.com>
2013-03-26 10:00:54 +0000

    CodeMetricTests refactoring
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@40 67bba403-d36b-46c0-a350-761a85a4c57d

--- VSProjects/Realisation/Weverca.CodeMetrics.UnitTest/IndicatorCodeMetricsTests.cs
+++ VSProjects/Realisation/Weverca.CodeMetrics.UnitTest/IndicatorCodeMetricsTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -9,39 +10,40 @@ namespace Weverca.CodeMetrics.UnitTest
     {
 
         #region Eval testing sources
-        readonly string EvalSimple = @"
-            eval('$x=3');
-        ";
-        readonly string EvalSimple_description = @"Test on eval detection outside function/method.";
+        readonly IEnumerable<SourceTest> EvalPositiveTests = new SourceTest[]{
+            new SourceTest("Test on eval detection outside function/method.", @"
+                eval('$x=3');
+            "),
 
-        readonly string EvalInFunction = @"
-            function test($param){
-                eval($param);
-            }
-        ";
-        readonly string EvalInFunction_description = @"Test on eval detection inside function declaration";
-
-        readonly string EvalInMethod = @"
-            class testClass{
-                function testMethod($param){
+            new SourceTest("Test on eval detection inside function declaration", @"
+                function test($param){
                     eval($param);
                 }
-            }
-        ";
-        readonly string EvalInMethod_description = @"Test on eval detection inside method declaration";
+            "),
+
+            new SourceTest("Test on eval detection inside method declaration", @"
+                class testClass{
+                    function testMethod($param){
+                        eval($param);
+                    }
+                }
+            ")
+        };
+
+        readonly IEnumerable<SourceTest> EvalNegativeTests = new SourceTest[]{
+            new SourceTest("No eval is present",TestingUtilities.HelloWorldSource)
+        };
+
         #endregion
 
         [TestMethod]
         public void Eval()
         {
-            var hasEval=TestingUtilities.GetContainsIndicatorPredicate(ConstructIndicator.Eval);
-            var doesntHaveEval=TestingUtilities.GetNegation(hasEval);
+            var hasEval = TestingUtilities.GetContainsIndicatorPredicate(ConstructIndicator.Eval);
+            var doesntHaveEval = TestingUtilities.GetNegation(hasEval);
 
-            TestingUtilities.RunTest(hasEval, EvalSimple, EvalSimple_description);
-            TestingUtilities.RunTest(hasEval, EvalInFunction, EvalInFunction_description);
-            TestingUtilities.RunTest(hasEval, EvalInMethod, EvalInMethod_description);
-
-            TestingUtilities.RunTest(doesntHaveEval, TestingUtilities.HelloWorldSource, "No eval is present");
+            TestingUtilities.RunTests(hasEval, EvalPositiveTests);     
+            TestingUtilities.RunTests(doesntHaveEval, EvalNegativeTests);
         }
     }
 }
--- VSProjects/Realisation/Weverca.CodeMetrics.UnitTest/TestingUtilities.cs
+++ VSProjects/Realisation/Weverca.CodeMetrics.UnitTest/TestingUtilities.cs
@@ -27,26 +27,75 @@ namespace Weverca.CodeMetrics.UnitTest
         /// Test given source according to predicate. On fail throws assertion error with testDescrpition        
         /// </summary>
         /// <param name="predicate"></param>
-        /// <param name="source"></param>
+        /// <param name="sourceCode"></param>
         /// <param name="testDescription"></param>
-        internal static void RunTest(MetricPredicate predicate, string source, string testDescription)
+        internal static void RunTest(MetricPredicate predicate, string sourceCode, string testDescription)
         {
             string fileName = "./file.php";
             PhpSourceFile source_file = new PhpSourceFile(new FullPath(Path.GetDirectoryName(fileName)), new FullPath(fileName));
-            var parser = new SyntaxParser(source_file,"<?php "+ source+" ?>");            
+            var parser = new SyntaxParser(source_file,"<?php "+ sourceCode+" ?>");            
             var metricInfo = MetricInfo.FromParsers(true, parser);
 
             Assert.IsTrue(predicate(metricInfo), testDescription);
         }
 
+        /// <summary>
+        /// Test given source tests against predicate. On fail throws assertion error with failed test description
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <param name="tests"></param>
+        internal static void RunTests(MetricPredicate predicate, IEnumerable<SourceTest> tests)
+        {
+            foreach (var test in tests)
+            {
+                RunTest(predicate, test.SourceCode, test.SourceCode);
+            }
+        }
+
+        /// <summary>
+        /// Get predicate for testing of presence given indicator
+        /// </summary>
+        /// <param name="indicator"></param>
+        /// <returns></returns>
         internal static MetricPredicate GetContainsIndicatorPredicate(ConstructIndicator indicator)
         {
             return (info) => info.HasIndicator(indicator);
         }
 
+        /// <summary>
+        /// Get negation to given predicate
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
         internal static MetricPredicate GetNegation(MetricPredicate predicate)
         {
             return (info) => !predicate(info);
+        }
+    }
+
+    /// <summary>
+    /// Represents test on source code
+    /// </summary>
+    class SourceTest
+    {
+        /// <summary>
+        /// Descrpition for this test.
+        /// </summary>
+        public readonly string Description;
+        /// <summary>
+        /// Source code for this test.
+        /// </summary>
+        public readonly string SourceCode;
+
+        /// <summary>
+        /// Create source test with given description and source code.
+        /// </summary>
+        /// <param name="description"></param>
+        /// <param name="sourceCode"></param>
+        public SourceTest(string description, string sourceCode)
+        {
+            this.Description = description;
+            this.SourceCode = sourceCode;
         }
     }
 }
