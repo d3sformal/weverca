50e3406408144325b60e6dd84250afbc33ba1ab4
vodolan <vodolan@mycompany.com>
2013-03-26 10:31:36 +0000

    CodeMetrics tests refactoring
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@43 67bba403-d36b-46c0-a350-761a85a4c57d

--- VSProjects/Realisation/Weverca.CodeMetrics.UnitTest/IndicatorCodeMetricsTests.cs
+++ VSProjects/Realisation/Weverca.CodeMetrics.UnitTest/IndicatorCodeMetricsTests.cs
@@ -36,22 +36,6 @@ namespace Weverca.CodeMetrics.UnitTest
 
         #endregion
 
-        #region Max depth inheritance tests
-        readonly IEnumerable<SourceTest> InheritanceDepth2 = new SourceTest[]{
-            new SourceTest("Simple class inheritance",@"
-                class A{
-                }
-                class B extends A{
-                }
-            "),
-
-        };
-
-        readonly IEnumerable<SourceTest> InheritanceDepth0 = new SourceTest[]{
-            new SourceTest("No type inheritance",TestingUtilities.HelloWorldSource)
-        };
-        #endregion
-
         [TestMethod]
         public void Eval()
         {
@@ -60,16 +44,6 @@ namespace Weverca.CodeMetrics.UnitTest
 
             TestingUtilities.RunTests(hasEval, EvalPositiveTests);
             TestingUtilities.RunTests(doesntHaveEval, EvalNegativeTests);
-        }
-
-        [TestMethod]
-        public void InheritanceDepth()
-        {
-            var inheritance0_predicate = TestingUtilities.GetQuantityPredicate(Quantity.MaxInheritanceDepth, 0);
-            var inheritance2_predicate = TestingUtilities.GetQuantityPredicate(Quantity.MaxInheritanceDepth, 2);
-
-            TestingUtilities.RunTests(inheritance0_predicate, InheritanceDepth0);
-            TestingUtilities.RunTests(inheritance2_predicate, InheritanceDepth2);
         }
     }
 }
---
+++ VSProjects/Realisation/Weverca.CodeMetrics.UnitTest/QuantityCodeMetricsTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Weverca.CodeMetrics.UnitTest
+{
+    [TestClass]
+    public class QuantityCodeMetricTests
+    {
+        
+        #region Max depth inheritance tests
+        readonly IEnumerable<SourceTest> InheritanceDepth2 = new SourceTest[]{
+            new SourceTest("Simple class inheritance",@"
+                class A{
+                }
+                class B extends A{
+                }
+            "),
+
+        };
+
+        readonly IEnumerable<SourceTest> InheritanceDepth0 = new SourceTest[]{
+            new SourceTest("No type inheritance",TestingUtilities.HelloWorldSource)
+        };
+        #endregion
+            
+
+        [TestMethod]
+        public void InheritanceDepth()
+        {
+            var inheritance0_predicate = TestingUtilities.GetQuantityPredicate(Quantity.MaxInheritanceDepth, 0);
+            var inheritance2_predicate = TestingUtilities.GetQuantityPredicate(Quantity.MaxInheritanceDepth, 2);
+
+            TestingUtilities.RunTests(inheritance0_predicate, InheritanceDepth0);
+            TestingUtilities.RunTests(inheritance2_predicate, InheritanceDepth2);
+        }
+    }
+}
