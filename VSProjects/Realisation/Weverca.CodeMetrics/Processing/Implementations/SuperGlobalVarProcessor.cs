0e5edafedc02cee29e23c2f417c4b223621d4a24
vodolan <vodolan@mycompany.com>
2013-03-31 18:54:26 +0000

    Implemented metric for super global variables
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@61 67bba403-d36b-46c0-a350-761a85a4c57d

---
+++ VSProjects/Realisation/Weverca.CodeMetrics/Processing/Implementations/SuperGlobalVarProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Weverca.Parsers;
+
+namespace Weverca.CodeMetrics.Processing.Implementations
+{
+    [Metric(ConstructIndicator.SuperGlobalVariable)]
+    class SuperGlobalVarProcessor:IndicatorProcessor
+    {
+        protected override IndicatorProcessor.Result process(bool resolveOccurances, ConstructIndicator category, SyntaxParser parser)
+        {
+            var visitor = new SuperGlobalVarVisitor();
+            parser.Ast.VisitMe(visitor);
+
+            var variables=visitor.GetVariables();
+            var hasOccurance = variables.Count() > 0;
+            return new Result(hasOccurance, variables);
+        }
+    }
+}
