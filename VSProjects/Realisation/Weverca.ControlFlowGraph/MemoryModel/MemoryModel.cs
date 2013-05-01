164345d19905984db8fa8a2c7c37ef2d85cd18b4
bastecky <bastecky@mycompany.com>
2013-05-01 14:40:41 +0000

    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@95 67bba403-d36b-46c0-a350-761a85a4c57d

--- VSProjects/Realisation/Weverca.ControlFlowGraph/MemoryModel/MemoryModel.cs
+++ VSProjects/Realisation/Weverca.ControlFlowGraph/MemoryModel/MemoryModel.cs
@@ -7,31 +7,129 @@ namespace Weverca.ControlFlowGraph.MemoryModel
 {
     class Table 
     {
-        public Dictionary<string, Variable> data;
-        Variable unknown;
+        Dictionary<string, Variable> data;
+        public Variable unknown;
+
         public Table()
         {
             data = new Dictionary<string, Variable>();
             unknown = null;
         }
+
+        public Variable GetOrCreateVariable(string variableName)
+        {
+            Variable res;
+            if (!data.TryGetValue(variableName, out res))
+            {
+                res = new Variable();
+                data[variableName] = res;
+            }
+            return res;
+        }
+
+        public bool TryGetVariable(string variableName,out Variable res) 
+        {
+            return data.TryGetValue(variableName, out res);
+        }
+
+        public void Remove(string variableName)
+        {
+            data.Remove(variableName);
+        }
+
+        public Variable this[string variableName]
+        {
+            get
+            {
+                return GetOrCreateVariable(variableName);
+            }
+        }
+
+
+        internal void Clear()
+        {
+            throw new NotImplementedException();
+        }
     }
 
-    class Variable 
+    class Variable
     {
-        List<Value> values;
-        Dictionary<string, Variable> mayAliases;
-        Dictionary<string, Variable> mustAliases;
-        //dva next level 1 pre objekty 1 pre polia???
-        Table nextLevel;
-        bool isUnknown;
+        public readonly List<Value> values;
+        public readonly HashSet<Variable> mayAliases;
+        public readonly HashSet<Variable> mustAliases;
+        public readonly Table nextLevel;
+        public readonly Variable previousLevel;
+
+        bool isAnyValue;
         public Variable() 
         {
-            isUnknown = false;
+            isAnyValue = false;
             nextLevel = new Table();
-            mayAliases = new Dictionary<string, Variable>();
-            mustAliases = new Dictionary<string, Variable>();
+            mayAliases = new HashSet<Variable>();
+            mustAliases = new HashSet<Variable>();
             values = new List<Value>();
         }
+
+        internal void SetValue(Value value)
+        {
+            setValue(value);
+
+            foreach (Variable must in mustAliases)
+            {
+                must.setValue(value);
+            }
+        }
+
+        private void setValue(Value value)
+        {
+            values.Clear();
+            nextLevel.Clear();
+            values.Add(value);
+
+            foreach (Variable may in mayAliases)
+            {
+                may.addValue(value);
+            }
+        }
+
+        private void addValue(Value value)
+        {
+            values.Add(value);
+
+            /*
+
+Test chování aliasů - při vytváření může vzniknout cyklus!!!!
+***********************PHP
+<?
+
+$v = array(1, 2, 3);
+$v[1] = array(4, 5, 6);
+
+echo "$v[0] $v[1] $v[2]<br>";
+echo $v[1][0]. " ".$v[1][1]. " ".$v[1][2]."<br>";
+
+if (!$_GET['t'])
+{
+    $v[1] = &$v;
+}
+
+echo "$v[0] $v[1] $v[2]<br>";
+echo $v[1][0]. " ".$v[1][1]. " ".$v[1][2]."<br>";
+?>
+
+<a href="alias.php?t=<?=!$_GET['t']?>" > test <?=$_GET['t']?></a>
+
+***********************PHP
+             
+             */
+        }
+    }
+
+    class ObjectReference : Value
+    {
+        Table table;
+        ClassDeclaration classInfo;
+        HashSet<Variable> referenced;
     }
 
     abstract class Value 
@@ -74,18 +172,42 @@ namespace Weverca.ControlFlowGraph.MemoryModel
 
         public void Merge(MemoryModel m) 
         {
-        
+        }
+
+        public Variable GetVariable(string variableName)
+        {
+            return table.GetOrCreateVariable(variableName);
+        }
+
+        public void SetVariableValue(string variableName, Value value)
+        {
+            Variable variable =  table.GetOrCreateVariable(variableName);
+            variable.SetValue(value);
+            
+        }
+
+        public Variable this[string variableName]
+        {
+            get
+            {
+                return table.GetOrCreateVariable(variableName);
+            }
         }
 
         //set value
         //ako resprezentovat $a["a"]["a"]["a"] 4 stringy?
-
+        //$a["a"]->a["a"]
         //set value to unknown
 
         //get value
 
         // remove value??
         public Object Clone()
+        {
+            return Copy();
+        }
+
+        public MemoryModel Copy()
         {
             throw new NotImplementedException();
         }
