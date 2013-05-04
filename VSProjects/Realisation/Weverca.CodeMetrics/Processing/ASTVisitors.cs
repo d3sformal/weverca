12b27ae7ea85ddc0b76ddba76e3642af20a72838
skorvaga <skorvaga@mycompany.com>
2013-05-04 23:27:48 +0000

    WEV-24: Add detection of type/function declaration within a subroutine
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@97 67bba403-d36b-46c0-a350-761a85a4c57d

--- VSProjects/Realisation/Weverca.CodeMetrics/Processing/ASTVisitors.cs
+++ VSProjects/Realisation/Weverca.CodeMetrics/Processing/ASTVisitors.cs
@@ -8,6 +8,36 @@ using PHP.Core.Reflection;
 namespace Weverca.CodeMetrics.Processing
 {
     /// <summary>
+    /// Enriched tree with a place to store AST nodes which represent some wanted language construct
+    /// </summary>
+    class OccurrenceTreeVisitor : TreeVisitor
+    {
+        /// <summary>
+        /// All occurrences of searched language constructs
+        /// </summary>
+        protected Stack<AstNode> occurrenceNodes = new Stack<AstNode>();
+
+        /// <summary>
+        /// Return all nodes with occurences of searched language constructs
+        /// </summary>
+        /// <returns>Occurrences of appropriate nodes</returns>
+        internal IEnumerable<AstNode> GetOccurrences()
+        {
+            // Copy result to an array to make it immutable
+            return occurrenceNodes.ToArray();
+        }
+
+        /// <summary>
+        /// Remove all stored occurrences
+        /// </summary>
+        internal void ResetContent()
+        {
+            occurrenceNodes.Clear();
+        }
+    }
+
+
+    /// <summary>
     /// Visitor which collect function calls
     /// </summary>
     class CallVisitor : TreeVisitor
@@ -67,6 +97,85 @@ namespace Weverca.CodeMetrics.Processing
         }
 
         #endregion
+    }
+
+
+    /// <summary>
+    /// Represents the visitor that finds all types declared in the body of a subroutine
+    /// </summary>
+    class InsideFunctionDeclarationVisitor : OccurrenceTreeVisitor
+    {
+        private delegate void VisitSubroutineExprDelegate<T>(T x) where T : AstNode;
+
+        /// <summary>
+        /// Indicate whether subtree of a subroutine is traversing at this point
+        /// </summary>
+        private bool isInsideSubroutine = false;
+
+        #region TreeVisitor overrides
+
+        public override void VisitMethodDecl(MethodDecl x)
+        {
+            VisitSubroutineExpr<MethodDecl>(base.VisitMethodDecl, x);
+        }
+
+        public override void VisitFunctionDecl(FunctionDecl x)
+        {
+            if (isInsideSubroutine)
+            {
+                occurrenceNodes.Push(x);
+            }
+            VisitSubroutineExpr<FunctionDecl>(base.VisitFunctionDecl, x);
+        }
+
+        public override void VisitLambdaFunctionExpr(LambdaFunctionExpr x)
+        {
+            VisitSubroutineExpr<LambdaFunctionExpr>(base.VisitLambdaFunctionExpr, x);
+        }
+
+        public override void VisitTypeDecl(TypeDecl x)
+        {
+            // Declaration of classes and interfaces too is non-standard construct
+            if (((x.AttributeTarget & PhpAttributeTargets.Types) != 0)
+                && (isInsideSubroutine))
+            {
+                // All classes and interfaces declared inside a function
+                occurrenceNodes.Push(x);
+            }
+
+            base.VisitTypeDecl(x);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Set that visitor is traversing subtree of declaration and body of a subroutine
+        /// </summary>
+        /// <typeparam name="T">The specific type of AST node</typeparam>
+        /// <param name="overriddenMethod">Method of AST which is overridden</param>
+        /// <param name="x">AST node of the subroutine declaration</param>
+        private void VisitSubroutineExpr<T>(VisitSubroutineExprDelegate<T>/*!*/ overriddenMethod,
+            T x) where T : AstNode
+        {
+            if (isInsideSubroutine)
+            {
+                overriddenMethod(x);
+                Debug.Assert(isInsideSubroutine);
+            }
+            else
+            {
+                isInsideSubroutine = true;
+                try
+                {
+                    overriddenMethod(x);
+                    Debug.Assert(isInsideSubroutine);
+                }
+                finally
+                {
+                    isInsideSubroutine = false;
+                }
+            }
+        }
     }
 
 
---
+++ VSProjects/Realisation/Weverca.CodeMetrics/Processing/Implementations/InsideFunctionDeclarationProcessor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+using Weverca.Parsers;
+
+namespace Weverca.CodeMetrics.Processing.Implementations
+{
+    /// <summary>
+    /// Identifies all classes and functions declared inside another subroutine
+    /// </summary>
+    [Metric(ConstructIndicator.InsideFunctionDeclaration)]
+    class InsideFunctionDeclarationProcessor : IndicatorProcessor
+    {
+        #region MetricProcessor overrides
+
+        protected override IndicatorProcessor.Result process(bool resolveOccurances,
+            ConstructIndicator category, SyntaxParser parser)
+        {
+            Debug.Assert(category == ConstructIndicator.InsideFunctionDeclaration);
+
+            // PHP.Core.AST.FunctionDecl.Function.IsIdentityDefinite may imply that
+            // if declaration is inside function, it is conditional (but not otherwise)
+            var visitor = new InsideFunctionDeclarationVisitor();
+            parser.Ast.VisitMe(visitor);
+
+            var occurrences = visitor.GetOccurrences();
+            var hasOccurrence = occurrences.GetEnumerator().MoveNext();
+            // Return classes (TypeDecl) and functions (FunctionDecl) declared inside a subroutine
+            return new Result(hasOccurrence, occurrences);
+        }
+
+        #endregion
+    }
+}
