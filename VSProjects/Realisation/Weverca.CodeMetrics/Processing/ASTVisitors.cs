5abb404df0e6c2f54c4e6d10d02acd42db93ef8b
skorvaga <skorvaga@mycompany.com>
2013-05-05 17:53:42 +0000

    WEV-25: Add detection of access to object members from variable-like construct
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@98 67bba403-d36b-46c0-a350-761a85a4c57d

--- VSProjects/Realisation/Weverca.CodeMetrics/Processing/ASTVisitors.cs
+++ VSProjects/Realisation/Weverca.CodeMetrics/Processing/ASTVisitors.cs
@@ -321,4 +321,100 @@ namespace Weverca.CodeMetrics.Processing
 
         #endregion
     }
+
+
+    /// <summary>
+    /// Represents the visitor that finds all accesses to members of objects whose type
+    /// cannot be resolved statically as they are stored within a variable
+    /// </summary>
+    class DuckTypingVisitor : OccurrenceTreeVisitor
+    {
+        #region TreeVisitor overrides
+
+        public override void VisitDirectVarUse(DirectVarUse x)
+        {
+            // Direct access to field: $object->field
+            FindUndefinedMembersUse(x);
+            base.VisitDirectVarUse(x);
+        }
+
+        public override void VisitIndirectVarUse(IndirectVarUse x)
+        {
+            // Access to field of a name stored in variable-like construct: $object->$field
+            FindUndefinedMembersUse(x);
+            base.VisitIndirectVarUse(x);
+        }
+
+        public override void VisitItemUse(ItemUse x)
+        {
+            // Access to array: $object->array[number]
+            FindUndefinedMembersUse(x);
+            base.VisitItemUse(x);
+        }
+
+        public override void VisitDirectFcnCall(DirectFcnCall x)
+        {
+            // Direct method call: $object->method()
+            FindUndefinedMembersUse(x);
+            base.VisitDirectFcnCall(x);
+        }
+
+        public override void VisitIndirectFcnCall(IndirectFcnCall x)
+        {
+            if (x.IsMemberOf != null)
+            {
+                // Variable method call: $object->$method()
+                FindUndefinedMembersUse(x);
+            }
+            else
+            {
+                // Or __invoke method call if variable is a object
+                occurrenceNodes.Push(x);
+            }
+            base.VisitIndirectFcnCall(x);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Find all possible undefined members of a object within the variable-like expression
+        /// </summary>
+        /// <param name="x">Variable-like expression possibly using members of a object</param>
+        private void FindUndefinedMembersUse(VarLikeConstructUse/*!*/ x)
+        {
+            // Every access to a field or method call can be seen as duck typing,
+            // because the type is not known without run-time information
+
+            if (x.IsMemberOf != null)
+            {
+                // The type of x is known to be proper
+                occurrenceNodes.Push(x);
+            }
+            else
+            {
+                return;
+            }
+
+            for (x = x.IsMemberOf; x.IsMemberOf != null; x = x.IsMemberOf)
+            {
+                // CompoundVarUse is supertype of DirectVarUse, IndirectVarUse and ItemUse
+                // DirectVarUse can be $this, detect by DirectVarUse.VarName.IsThisVariableName
+                if ((x is CompoundVarUse)
+                    || (x is DirectFcnCall)
+                    || (x is IndirectFcnCall))
+                {
+                    // Variable-like construct is possible member of a object
+                    occurrenceNodes.Push(x);
+                }
+            }
+
+            if (x is IndirectFcnCall)
+            {
+                // __invoke method call if call is not member of a object. Otherwise call of __invoke
+                // declared within object stored in a field is handled as method call
+                occurrenceNodes.Push(x);
+            }
+        }
+    }
+
 }
---
+++ VSProjects/Realisation/Weverca.CodeMetrics/Processing/Implementations/DuckTypingProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+using Weverca.Parsers;
+
+namespace Weverca.CodeMetrics.Processing.Implementations
+{
+    /// <summary>
+    /// Identifies all occurrences of members of objects which are used dynamically as variable
+    /// </summary>
+    [Metric(ConstructIndicator.DuckTyping)]
+    class DuckTypingProcessor : IndicatorProcessor
+    {
+        #region MetricProcessor overrides
+
+        protected override IndicatorProcessor.Result process(bool resolveOccurances,
+            ConstructIndicator category, SyntaxParser parser)
+        {
+            Debug.Assert(category == ConstructIndicator.DuckTyping);
+
+            var visitor = new DuckTypingVisitor();
+            parser.Ast.VisitMe(visitor);
+
+            var occurrences = visitor.GetOccurrences();
+            var hasOccurrence = occurrences.GetEnumerator().MoveNext();
+            // Return all variable-like constructs (VarLikeConstructUse) with use of member of unknown object
+            return new Result(hasOccurrence, occurrences);
+        }
+
+        #endregion
+    }
+}
