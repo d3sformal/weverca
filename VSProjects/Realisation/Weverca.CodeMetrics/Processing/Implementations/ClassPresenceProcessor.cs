bd7c28cb570b04d08bddc05c468c27a832a78641
brenner <brenner@mycompany.com>
2013-05-08 09:49:00 +0000

    WEV-5 / WEV-14 implementace Class Presence metriky
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@101 67bba403-d36b-46c0-a350-761a85a4c57d

---
+++ VSProjects/Realisation/Weverca.CodeMetrics/Processing/Implementations/ClassPresenceProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PHP.Core.AST;
+
+namespace Weverca.CodeMetrics.Processing.Implementations
+{
+    /// <summary>
+    /// Determines whether there is class presence.
+    /// </summary>
+    [Metric(ConstructIndicator.Class)]
+    class ClassPresenceProcessor : IndicatorProcessor
+    {
+        #region IndicatorProcessor overrides
+
+        protected override Result process(bool resolveOccurances, ConstructIndicator category, Parsers.SyntaxParser parser)
+        {
+            Debug.Assert(category == ConstructIndicator.Class);
+            Debug.Assert(parser.IsParsed);
+            Debug.Assert(!parser.Errors.AnyError);
+
+            var classes = parser.Types.Where(t => !t.Value.IsInterface);
+            bool classPresent = classes.Count() > 0;
+
+            var occurences = classes.Select(t => t.Value.Declaration.GetNode() as TypeDecl).Where(t => t != null).ToArray(); // they all should not be null
+
+            return new Result(classPresent, occurences);
+        }
+
+        #endregion
+    }
+}
