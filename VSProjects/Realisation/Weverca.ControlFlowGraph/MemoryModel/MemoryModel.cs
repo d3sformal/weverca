827cc9fc471fb22aed23a2d7859e3bea6645f194
bastecky <bastecky@mycompany.com>
2013-05-09 16:59:39 +0000

    Memory model - implementace aliasů, kopírování a částečně merge.
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@116 67bba403-d36b-46c0-a350-761a85a4c57d

--- VSProjects/Realisation/Weverca.ControlFlowGraph/MemoryModel/MemoryModel.cs
+++ VSProjects/Realisation/Weverca.ControlFlowGraph/MemoryModel/MemoryModel.cs
@@ -5,164 +5,11 @@ using System.Text;
 
 namespace Weverca.ControlFlowGraph.MemoryModel
 {
-    class Table 
-    {
-        Dictionary<string, Variable> data;
-        public Variable unknown;
-
-        public Table()
-        {
-            data = new Dictionary<string, Variable>();
-            unknown = null;
-        }
-
-        public Variable GetOrCreateVariable(string variableName)
-        {
-            Variable res;
-            if (!data.TryGetValue(variableName, out res))
-            {
-                res = new Variable();
-                data[variableName] = res;
-            }
-            return res;
-        }
-
-        public bool TryGetVariable(string variableName,out Variable res) 
-        {
-            return data.TryGetValue(variableName, out res);
-        }
-
-        public void Remove(string variableName)
-        {
-            data.Remove(variableName);
-        }
-
-        public Variable this[string variableName]
-        {
-            get
-            {
-                return GetOrCreateVariable(variableName);
-            }
-        }
-
-
-        internal void Clear()
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    class Variable
-    {
-        public readonly List<Value> values;
-        public readonly HashSet<Variable> mayAliases;
-        public readonly HashSet<Variable> mustAliases;
-        public readonly Table nextLevel;
-        public readonly Variable previousLevel;
-
-        bool isAnyValue;
-        public Variable() 
-        {
-            isAnyValue = false;
-            nextLevel = new Table();
-            mayAliases = new HashSet<Variable>();
-            mustAliases = new HashSet<Variable>();
-            values = new List<Value>();
-        }
-
-        internal void SetValue(Value value)
-        {
-            setValue(value);
-
-            foreach (Variable must in mustAliases)
-            {
-                must.setValue(value);
-            }
-        }
-
-        private void setValue(Value value)
-        {
-            values.Clear();
-            nextLevel.Clear();
-            values.Add(value);
-
-            foreach (Variable may in mayAliases)
-            {
-                may.addValue(value);
-            }
-        }
-
-        private void addValue(Value value)
-        {
-            values.Add(value);
-
-            /*
-
-Test chování aliasů - při vytváření může vzniknout cyklus!!!!
-***********************PHP
-<?
-
-$v = array(1, 2, 3);
-$v[1] = array(4, 5, 6);
-
-echo "$v[0] $v[1] $v[2]<br>";
-echo $v[1][0]. " ".$v[1][1]. " ".$v[1][2]."<br>";
-
-if (!$_GET['t'])
-{
-    $v[1] = &$v;
-}
-
-echo "$v[0] $v[1] $v[2]<br>";
-echo $v[1][0]. " ".$v[1][1]. " ".$v[1][2]."<br>";
-?>
-
-<a href="alias.php?t=<?=!$_GET['t']?>" > test <?=$_GET['t']?></a>
-
-***********************PHP
-             
-             */
-        }
-    }
-
-    class ObjectReference : Value
-    {
-        Table table;
-        ClassDeclaration classInfo;
-        HashSet<Variable> referenced;
-    }
-
-    abstract class Value 
-    { 
     
-    }
-
-    class IntegerValue : Value 
-    {
-        public int value;
-    }
-
-    class StringValue : Value 
-    {
-        public string value;
     
-    }
+    
 
-    class BooleanValue : Value
-    {
-        public bool value;
-    }
-
-    class FloatValue : Value
-    {
-        public double value;
-    }
-
-    class UndefinedValue : Value
-    {
-    }
-
-    class MemoryModel : ICloneable
+    class MemoryModel
     {
         private Table table;
         public MemoryModel()
@@ -173,42 +20,58 @@ echo $v[1][0]. " ".$v[1][1]. " ".$v[1][2]."<br>";
         public void Merge(MemoryModel m) 
         {
         }
-
-        public Variable GetVariable(string variableName)
+        
+        public MemoryModel Copy()
         {
-            return table.GetOrCreateVariable(variableName);
-        }
+            MemoryModel newMemd = new MemoryModel();
 
-        public void SetVariableValue(string variableName, Value value)
-        {
-            Variable variable =  table.GetOrCreateVariable(variableName);
-            variable.SetValue(value);
+            CopyResolver resolver = new CopyResolver();
             
+            newMemd.table.CopyStructureFrom(table, resolver);
+            resolver.ResolveAliases();
+
+            return newMemd;
         }
-        public Variable this[string variableName]
+
+        public void MergeCopy(MemoryModel source)
         {
-            get
+            CopyResolver resolver = new CopyResolver();
+            table.MergeCopy(source.table, resolver);
+        }
+     }
+
+    class CopyResolver
+    {
+
+        Dictionary<Variable, Variable> resolveTable = new Dictionary<Variable, Variable>();
+
+        internal void ResolveAliases()
+        {
+            foreach (var pair in resolveTable)
             {
-                return table.GetOrCreateVariable(variableName);
+                Variable oldVar = pair.Key;
+                Variable newVar = pair.Value;
+
+                foreach (Variable oldAlias in oldVar.mayAliases)
+                {
+                    Variable newAlias = resolveTable[oldAlias];
+                    newVar.mayAliases.Add(newAlias);
+                }
+
+                if (oldVar.HasMustAliases && oldVar.IsRepresentant)
+                {
+                    foreach (Variable oldAlias in oldVar.mustAliases)
+                    {
+                        Variable newAlias = resolveTable[oldAlias];
+                        newAlias.setResolvedAlias(newVar);
+                    }
+                }
             }
         }
 
-        //set value
-        //ako resprezentovat $a["a"]["a"]["a"] 4 stringy?
-        //$a["a"]->a["a"]
-        //set value to unknown
-
-        //get value
-
-        // remove value??
-        public Object Clone()
+        internal void Add(Variable newVar, Variable oldVar)
         {
-            return Copy();
+            resolveTable[oldVar] = newVar;
         }
-
-        public MemoryModel Copy()
-        {
-            throw new NotImplementedException();
-        }
-     }
+    }
 }
