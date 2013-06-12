0d018f4aaad7d801b9ef9a79956a519adabdad36
bastecky <bastecky@mycompany.com>
2013-06-12 10:03:27 +0000

    Změna struktury původního memory modelu
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@140 67bba403-d36b-46c0-a350-761a85a4c57d

---
+++ VSProjects/Realisation/Weverca.ControlFlowGraph/MemoryModel/MemorySnapshot.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using PHP.Core;
+using Weverca.ControlFlowGraph.Analysis.Memory;
+
+namespace Weverca.ControlFlowGraph.MemoryModel
+{
+    class MemorySnapshot : AbstractSnapshot
+    {
+        protected override void startTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool commitTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override ObjectValue createObject()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override AssociativeArray createArray()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override AliasValue createAlias(PHP.Core.VariableName sourceVar)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override AbstractSnapshot createCall(Analysis.CallInfo callInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void assign(PHP.Core.VariableName targetVar, MemoryEntry entry)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void assignAlias(PHP.Core.VariableName targetVar, AliasValue alias)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void extend(ISnapshotReadonly[] inputs)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void mergeWithCall(Analysis.CallResult result, ISnapshotReadonly callOutput)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override MemoryEntry readValue(PHP.Core.VariableName sourceVar)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
