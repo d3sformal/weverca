67ad4a60f07317f56fc85a868da4341b8c355014
kikta <kikta@mycompany.com>
2013-06-30 14:46:26 +0000

    pridana kostra pre native function analyzer
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@170 67bba403-d36b-46c0-a350-761a85a4c57d

---
+++ VSProjects/Realisation/Weverca.TaintedAnalysis/NativeFunctionAnalyzer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Weverca.Analysis;
+using PHP.Core;
+
+namespace Weverca.TaintedAnalysis
+{
+    class NativeFunctionAnalyzer
+    {
+        bool existNativeFunction(QualifiedName name)
+        {
+
+            return false;
+        }
+        QualifiedName[] getNativeFunctions()
+        {
+            return null;
+        }
+        NativeAnalyzer getNativeAnalyzer(QualifiedName name)
+        {
+            return null;
+        }
+
+    }
+    interface NativeAnalyzer
+    {
+        void analyze(FlowController flow) { }
+    }
+}
