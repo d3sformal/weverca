6b127543ce1cec1070892c6dfa8db65851cebaeb
skorvaga <skorvaga@mycompany.com>
2013-07-06 14:03:48 +0000

    WEV-32: Add function coupling measurement
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@181 67bba403-d36b-46c0-a350-761a85a4c57d

---
+++ VSProjects/Realisation/Weverca.CodeMetrics/Processing/ASTVisitors/FunctionCouplingVisitor.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections.Generic;
+
+using PHP.Core;
+using PHP.Core.AST;
+using PHP.Core.Reflection;
+
+namespace Weverca.CodeMetrics.Processing.ASTVisitors
+{
+    /// <summary>
+    /// Represents the visitor that finds all couplings from a given standard function
+    /// </summary>
+    class FunctionCouplingVisitor : TreeVisitor
+    {
+        /// <summary>
+        /// Standard function whose subtree is traversed for function references at this point
+        /// </summary>
+        private readonly PhpFunction currentFunction;
+        /// <summary>
+        /// References to other functions used within the current function body
+        /// </summary>
+        private readonly Dictionary<QualifiedName, DirectFcnCall> currentReferences
+            = new Dictionary<QualifiedName, DirectFcnCall>();
+        /// <summary>
+        /// Indicate whether function subtree is traversing. No inside function is accessed
+        /// </summary>
+        private bool isInsideFunction = false;
+
+        public FunctionCouplingVisitor(PhpFunction/*!*/ function)
+        {
+            currentFunction = function;
+        }
+
+        /// <summary>
+        /// Return all nodes with references to functions that occur within a given function body
+        /// </summary>
+        /// <returns>References of functions within a given function body</returns>
+        public DirectFcnCall[] GetReferences()
+        {
+            var couplings = new DirectFcnCall[currentReferences.Count];
+            // Copy result to an array to make it immutable
+            currentReferences.Values.CopyTo(couplings, 0);
+            return couplings;
+        }
+
+        #region TreeVisitor overrides
+
+        public override void VisitFunctionDecl(FunctionDecl x)
+        {
+            // The function can be declared inside other function or method body
+            // In that case, traversing the tree of the inside function is not necessary
+            if (!isInsideFunction)
+            {
+                var previousValue = isInsideFunction;
+                try
+                {
+                    isInsideFunction = true;
+                    currentReferences.Clear();
+                    base.VisitFunctionDecl(x);
+                }
+                finally
+                {
+                    isInsideFunction = previousValue;
+                }
+            }
+        }
+
+        public override void VisitDirectVarUse(DirectVarUse x)
+        {
+            // Direct access to field: $object->field
+            FindFunctionCall(x);
+            base.VisitDirectVarUse(x);
+        }
+
+        public override void VisitIndirectVarUse(IndirectVarUse x)
+        {
+            // Access to field of a name stored in variable-like construct: $object->$field
+            FindFunctionCall(x);
+            base.VisitIndirectVarUse(x);
+        }
+
+        public override void VisitItemUse(ItemUse x)
+        {
+            // Access to array: $object->array[number]
+            FindFunctionCall(x);
+            base.VisitItemUse(x);
+        }
+
+        public override void VisitDirectFcnCall(DirectFcnCall x)
+        {
+            // Direct method call: $object->method()
+            FindFunctionCall(x);
+            base.VisitDirectFcnCall(x);
+        }
+
+        public override void VisitIndirectFcnCall(IndirectFcnCall x)
+        {
+            // Variable method call: $object->$method()
+            FindFunctionCall(x);
+            base.VisitIndirectFcnCall(x);
+        }
+
+        #endregion
+
+        private void FindFunctionCall(VarLikeConstructUse/*!*/ x)
+        {
+            // Function is not member of anything
+            for (; x.IsMemberOf != null; x = x.IsMemberOf) {}
+
+            if (x is DirectFcnCall)
+            {
+                var call = x as DirectFcnCall;
+                AddCoupling(call);
+            }
+        }
+
+        /// <summary>
+        /// Add found reference to a function used within the currently traversed function body
+        /// </summary>
+        /// <param name="x">Reference to standard function</param>
+        private void AddCoupling(DirectFcnCall/*!*/ x)
+        {
+            Debug.Assert(isInsideFunction);
+
+            // Presence of itself is not taken into account
+            // Function names are case-insensitive
+            if (!x.QualifiedName.Equals(currentFunction.QualifiedName))
+            {
+                currentReferences[x.QualifiedName] = x;
+            }
+        }
+    }
+}
---
+++ VSProjects/Realisation/Weverca.CodeMetrics/Processing/Implementations/FunctionCouplingProcessor.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+using PHP.Core.AST;
+using PHP.Core.Reflection;
+
+using Weverca.CodeMetrics.Processing.ASTVisitors;
+using Weverca.Parsers;
+
+namespace Weverca.CodeMetrics.Processing.Implementations
+{
+    /// <summary>
+    /// Calculates how many user-defined functions a single function calls on average
+    /// </summary>
+    [Metric(Rating.PhpFunctionsCoupling)]
+    class FunctionCouplingProcessor : RatingProcessor
+    {
+        #region MetricProcessor overrides
+
+        protected override Result process(bool resolveOccurances, Rating category,
+            SyntaxParser parser)
+        {
+            Debug.Assert(category == Rating.PhpFunctionsCoupling);
+            Debug.Assert(parser.IsParsed);
+            Debug.Assert(!parser.Errors.AnyError);
+
+            if (parser.Functions == null)
+            {
+                // No function is declared
+                if (resolveOccurances)
+                {
+                    return new Result(0.0, new DirectFcnCall[0]);
+                }
+                else
+                {
+                    return new Result(0.0);
+                }
+            }
+
+            var functionReferences = new Stack<KeyValuePair<FunctionDecl, DirectFcnCall[]>>();
+            foreach (var routine in parser.Functions)
+            {
+                Debug.Assert(routine.Value.Member is PhpFunction);
+                var phpFunction = routine.Value.Member as PhpFunction;
+
+                Debug.Assert(phpFunction.Declaration.GetNode() is FunctionDecl);
+                var declaration = phpFunction.Declaration.GetNode() as FunctionDecl;
+
+                var visitor = new FunctionCouplingVisitor(declaration.Function);
+                declaration.VisitMe(visitor);
+                var references = visitor.GetReferences();
+                functionReferences.Push(new KeyValuePair<FunctionDecl, DirectFcnCall[]>(
+                    declaration, references));
+            }
+
+            var result = CalculateRating(functionReferences.ToArray());
+
+            if (resolveOccurances)
+            {
+                var allFunctionReferences = new Stack<DirectFcnCall>();
+                foreach (var functionReference in functionReferences)
+                {
+                    foreach (var reference in functionReference.Value)
+                    {
+                        allFunctionReferences.Push(reference);
+                    }
+                }
+
+                // Return all function calls (DirectFcnCall) used inside a function body
+                return new Result(result, allFunctionReferences.ToArray());
+            }
+            else
+            {
+                return new Result(result);
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Calculate average number of unique function references that a function contains
+        /// </summary>
+        /// <returns>
+        /// Measurement of average number of unique function reference inside a function implementation
+        /// </returns>
+        private double CalculateRating(KeyValuePair<FunctionDecl, DirectFcnCall[]>[]/*!*/ functionCouplings)
+        {
+            if (functionCouplings.Length <= 0)
+            {
+                return 0.0;
+            }
+
+            var numberOfReferences = 0;
+            foreach (var functionDeclaration in functionCouplings)
+            {
+                Debug.Assert(functionDeclaration.Value != null);
+                numberOfReferences += functionDeclaration.Value.Length;
+            }
+            return System.Convert.ToDouble(numberOfReferences) / functionCouplings.Length;
+        }
+    }
+}
