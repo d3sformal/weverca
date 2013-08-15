fc40c691b88a92b10bdb6bcb8184ee0cdc031444
bastecky <bastecky@mycompany.com>
2013-08-15 17:47:31 +0000

    Missing files added
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@240 67bba403-d36b-46c0-a350-761a85a4c57d

---
+++ VSProjects/Realisation/Weverca.MemoryModels/MemoryModel/DestroyemoryEntryVisitor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Weverca.Analysis.Memory;
+
+namespace Weverca.MemoryModels.MemoryModel.ValueVisitors
+{
+    class DestroyemoryEntryVisitor : AbstractValueVisitor
+    {
+        private Snapshot snapshot;
+        private MemoryIndex index;
+
+        public DestroyemoryEntryVisitor(Snapshot snapshot, MemoryIndex index)
+        {
+            this.snapshot = snapshot;
+            this.index = index;
+        }
+
+        public override void VisitValue(Value value)
+        {
+        }
+
+        public override void VisitAssociativeArray(AssociativeArray value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void VisitObjectValue(ObjectValue value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VSProjects/Realisation/Weverca.MemoryModels/MemoryModel/IndexMemoryInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Weverca.MemoryModels.MemoryModel
+{
+    class IndexMemoryInfo : MemoryInfo
+    {
+        private Analysis.Memory.AssociativeArray value;
+
+        public IndexMemoryInfo(MemoryIndex index, Analysis.Memory.AssociativeArray value)
+            : base(index)
+        {
+            this.value = value;
+        }
+    }
+}
