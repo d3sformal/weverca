3b06bf01f6ae7208869a963ca35d54856223ee50
brenner <brenner@mycompany.com>
2013-08-18 10:30:52 +0000

    WEV-31 přidání InternalsVisibleTo pro účely testování FlowResolveru
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@248 67bba403-d36b-46c0-a350-761a85a4c57d

--- VSProjects/Realisation/Weverca.Analysis/Properties/AssemblyInfo.cs
+++ VSProjects/Realisation/Weverca.Analysis/Properties/AssemblyInfo.cs
@@ -13,6 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: InternalsVisibleTo("Weverca.TaintedAnalysis.UnitTest")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
