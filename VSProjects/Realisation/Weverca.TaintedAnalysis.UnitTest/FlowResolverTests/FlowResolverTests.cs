21c591e9883747c0acd755ed01e535ccea56af5b
brenner <brenner@mycompany.com>
2013-08-20 17:28:58 +0000

    WEV-31 Testy pro primitivní typy
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@261 67bba403-d36b-46c0-a350-761a85a4c57d

--- VSProjects/Realisation/Weverca.TaintedAnalysis.UnitTest/FlowResolverTests/FlowResolverTests.cs
+++ VSProjects/Realisation/Weverca.TaintedAnalysis.UnitTest/FlowResolverTests/FlowResolverTests.cs
@@ -31,5 +31,37 @@ namespace Weverca.TaintedAnalysis.UnitTest.FlowResolverTests
                 .AddResult(ConditionForm.None, true, ConditionResults.False).AddResultValue("a", new AnyValue())
                 .Run();
         }
+
+        [TestMethod]
+        public void DirectVarEqualsFloat()
+        {
+            TestCase.Create(new BinaryEx(Operations.Equal, new DirectVarUse(new Position(), new VariableName("a")), new DoubleLiteral(new Position(), 1.1)))
+                .AddResult(ConditionForm.All, true, ConditionResults.True).AddResultValue("a", new FloatValue(1.1))
+                .AddResult(ConditionForm.None, false, ConditionResults.True)
+                .AddResult(ConditionForm.None, true, ConditionResults.False).AddResultValue("a", new AnyValue())
+                .Run();
+        }
+
+        [TestMethod]
+        public void DirectVarEqualsBool()
+        {
+            TestCase.Create(new BinaryEx(Operations.Equal, new DirectVarUse(new Position(), new VariableName("a")), new BoolLiteral(new Position(), true)))
+                .AddResult(ConditionForm.All, true, ConditionResults.True).AddResultValue("a", new BooleanValue(true))
+                .AddResult(ConditionForm.None, false, ConditionResults.True)
+                .AddResult(ConditionForm.None, true, ConditionResults.False).AddResultValue("a", new AnyValue())
+                .Run();
+        }
+
+        [TestMethod]
+        public void DirectVarEqualsNull()
+        {
+            //TODO: "NullValue"
+            
+            //TestCase.Create(new BinaryEx(Operations.Equal, new DirectVarUse(new Position(), new VariableName("a")), new NullLiteral(new Position())))
+            //    .AddResult(ConditionForm.All, true, ConditionResults.True).AddResultValue("a", new value)
+            //    .AddResult(ConditionForm.None, false, ConditionResults.True)
+            //    .AddResult(ConditionForm.None, true, ConditionResults.False).AddResultValue("a", new AnyValue())
+            //    .Run();
+        }
     }
 }
