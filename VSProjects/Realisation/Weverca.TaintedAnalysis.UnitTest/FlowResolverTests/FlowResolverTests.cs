dd8809f0db4aadabf059db66c6cfb4c0a8d8b6cb
brenner <brenner@mycompany.com>
2013-08-20 21:17:26 +0000

    WEV-31 Přidány testy
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@263 67bba403-d36b-46c0-a350-761a85a4c57d

--- VSProjects/Realisation/Weverca.TaintedAnalysis.UnitTest/FlowResolverTests/FlowResolverTests.cs
+++ VSProjects/Realisation/Weverca.TaintedAnalysis.UnitTest/FlowResolverTests/FlowResolverTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using PHP.Core;
@@ -13,89 +14,67 @@ namespace Weverca.TaintedAnalysis.UnitTest.FlowResolverTests
     [TestClass]
     public class FlowResolverTests
     {
-        [TestMethod]
-        public void DirectVarEqualsString()
+        // variable value, expected variable value in assumed block
+        static List<Tuple<Literal, Value>> equalTests = new List<Tuple<Literal, Value>>();
+
+        static FlowResolverTests()
         {
-            TestCase.Create(new BinaryEx(Operations.Equal, new DirectVarUse(new Position(), new VariableName("a")), ValueFactory.Create("aaa")))
-                .AddResult(ConditionForm.All, true, ConditionResults.True).AddResultValue("a", new StringValue("aaa"))
-                .AddResult(ConditionForm.None, false, ConditionResults.True)
-                .AddResult(ConditionForm.None, true, ConditionResults.False).AddResultValue("a", new AnyValue())
-                .Run();
+            equalTests.Add(LiteralValueFactory.Create("aaa"));
+            equalTests.Add(LiteralValueFactory.Create(1));
+            equalTests.Add(LiteralValueFactory.Create(1L));
+            equalTests.Add(LiteralValueFactory.Create(1.1));
+            equalTests.Add(LiteralValueFactory.Create(true));
+        }
+        
+        [TestMethod]
+        public void DirectVarEquals()
+        {
+            string variableName = "a";
+
+            foreach (var test in equalTests)
+            {
+                TestCase.Create(new BinaryEx(Operations.Equal, new DirectVarUse(new Position(), new VariableName(variableName)), test.Item1))
+                    .AddResult(ConditionForm.All, true, ConditionResults.True).AddResultValue(variableName, test.Item2)
+                    .AddResult(ConditionForm.None, false, ConditionResults.True)
+                    .AddResult(ConditionForm.None, true, ConditionResults.False).AddResultValue(variableName, new AnyValue())
+                    .Run();
+
+                //flipped order: var == value --> value == var
+                TestCase.Create(new BinaryEx(Operations.Equal, test.Item1, new DirectVarUse(new Position(), new VariableName(variableName))))
+                    .AddResult(ConditionForm.All, true, ConditionResults.True).AddResultValue(variableName, test.Item2)
+                    .AddResult(ConditionForm.None, false, ConditionResults.True)
+                    .AddResult(ConditionForm.None, true, ConditionResults.False).AddResultValue(variableName, new AnyValue())
+                    .Run();
+            }
         }
 
         [TestMethod]
-        public void DirectVarEqualsInt()
+        public void DirectVarNotEquals()
         {
-            TestCase.Create(new BinaryEx(Operations.Equal, ValueFactory.Create(1), new DirectVarUse(new Position(), new VariableName("a"))))
-                .AddResult(ConditionForm.All, true, ConditionResults.True).AddResultValue("a", new IntegerValue(1))
-                .AddResult(ConditionForm.None, false, ConditionResults.True)
-                .AddResult(ConditionForm.None, true, ConditionResults.False).AddResultValue("a", new AnyValue())
-                .Run();
-        }
+            string variableName = "a";
 
-        [TestMethod]
-        public void DirectVarEqualsFloat()
-        {
-            TestCase.Create(new BinaryEx(Operations.Equal, new DirectVarUse(new Position(), new VariableName("a")), ValueFactory.Create(1.1)))
-                .AddResult(ConditionForm.All, true, ConditionResults.True).AddResultValue("a", new FloatValue(1.1))
-                .AddResult(ConditionForm.None, false, ConditionResults.True)
-                .AddResult(ConditionForm.None, true, ConditionResults.False).AddResultValue("a", new AnyValue())
-                .Run();
-        }
+            foreach (var test in equalTests)
+            {
+                TestCase.Create(new BinaryEx(Operations.NotEqual, new DirectVarUse(new Position(), new VariableName(variableName)), test.Item1))
+                    .AddResult(ConditionForm.All, true, ConditionResults.True).AddResultValue("a", new AnyValue())
+                    .AddResult(ConditionForm.None, false, ConditionResults.True)
+                    .AddResult(ConditionForm.None, true, ConditionResults.False).AddResultValue("a", test.Item2)
+                    .Run();
 
-        [TestMethod]
-        public void DirectVarEqualsBool()
-        {
-            TestCase.Create(new BinaryEx(Operations.Equal, new DirectVarUse(new Position(), new VariableName("a")), ValueFactory.Create(true)))
-                .AddResult(ConditionForm.All, true, ConditionResults.True).AddResultValue("a", new BooleanValue(true))
-                .AddResult(ConditionForm.None, false, ConditionResults.True)
-                .AddResult(ConditionForm.None, true, ConditionResults.False).AddResultValue("a", new AnyValue())
-                .Run();
-        }
-
-        [TestMethod]
-        public void DirectVarNotEqualsString()
-        {
-            TestCase.Create(new BinaryEx(Operations.NotEqual, new DirectVarUse(new Position(), new VariableName("a")), ValueFactory.Create("aaa")))
-                .AddResult(ConditionForm.All, true, ConditionResults.True).AddResultValue("a", new AnyValue())
-                .AddResult(ConditionForm.None, false, ConditionResults.True)
-                .AddResult(ConditionForm.None, true, ConditionResults.False).AddResultValue("a", new StringValue("aaa"))
-                .Run();
-        }
-
-        [TestMethod]
-        public void DirectVarNotEqualsInt()
-        {
-            TestCase.Create(new BinaryEx(Operations.NotEqual, new DirectVarUse(new Position(), new VariableName("a")), ValueFactory.Create(1)))
-                .AddResult(ConditionForm.All, true, ConditionResults.True).AddResultValue("a", new AnyValue())
-                .AddResult(ConditionForm.None, false, ConditionResults.True)
-                .AddResult(ConditionForm.None, true, ConditionResults.False).AddResultValue("a", new IntegerValue(1))
-                .Run();
-        }
-
-        [TestMethod]
-        public void DirectVarNotEqualsFloat()
-        {
-            TestCase.Create(new BinaryEx(Operations.NotEqual, new DirectVarUse(new Position(), new VariableName("a")), ValueFactory.Create(1.1)))
-                .AddResult(ConditionForm.All, true, ConditionResults.True).AddResultValue("a", new AnyValue())
-                .AddResult(ConditionForm.None, false, ConditionResults.True)
-                .AddResult(ConditionForm.None, true, ConditionResults.False).AddResultValue("a", new FloatValue(1.1))
-                .Run();
-        }
-
-        [TestMethod]
-        public void DirectVarNotEqualsBool()
-        {
-            TestCase.Create(new BinaryEx(Operations.NotEqual, new DirectVarUse(new Position(), new VariableName("a")), ValueFactory.Create(true)))
-                .AddResult(ConditionForm.All, true, ConditionResults.True).AddResultValue("a", new AnyValue())
-                .AddResult(ConditionForm.None, false, ConditionResults.True)
-                .AddResult(ConditionForm.None, true, ConditionResults.False).AddResultValue("a", new BooleanValue(true))
-                .Run();
+                //flipped order: var != value --> value != var
+                TestCase.Create(new BinaryEx(Operations.NotEqual, test.Item1, new DirectVarUse(new Position(), new VariableName(variableName))))
+                    .AddResult(ConditionForm.All, true, ConditionResults.True).AddResultValue("a", new AnyValue())
+                    .AddResult(ConditionForm.None, false, ConditionResults.True)
+                    .AddResult(ConditionForm.None, true, ConditionResults.False).AddResultValue("a", test.Item2)
+                    .Run();
+            }
         }
 
         [TestMethod]
         public void DirectVarEqualsNull()
         {
+            Assert.Fail();
+            
             //TODO: "NullValue"
             
             //TestCase.Create(new BinaryEx(Operations.Equal, new DirectVarUse(new Position(), new VariableName("a")), new NullLiteral(new Position())))
@@ -117,20 +96,21 @@ namespace Weverca.TaintedAnalysis.UnitTest.FlowResolverTests
         }
 
         [TestMethod]
-        public void BinaryArithmeticsExpression()
+        public void BinaryArithmeticsExpressions()
         {
+            //BinaryEx.Operator, left operane, right operand, result of the operation
             Tuple<Operations, Literal, Literal, Literal>[] tests = new Tuple<Operations, Literal, Literal, Literal>[]
             {
-                new Tuple<Operations, Literal, Literal, Literal>(Operations.Add, ValueFactory.Create(2), ValueFactory.Create(3), ValueFactory.Create(5)),
-                new Tuple<Operations, Literal, Literal, Literal>(Operations.Sub, ValueFactory.Create(5), ValueFactory.Create(3), ValueFactory.Create(2)),
-                new Tuple<Operations, Literal, Literal, Literal>(Operations.Mul, ValueFactory.Create(2), ValueFactory.Create(3), ValueFactory.Create(6)),
-                new Tuple<Operations, Literal, Literal, Literal>(Operations.Div, ValueFactory.Create(6), ValueFactory.Create(3), ValueFactory.Create(2)),
-                new Tuple<Operations, Literal, Literal, Literal>(Operations.BitAnd, ValueFactory.Create(1), ValueFactory.Create(1), ValueFactory.Create(1)),
-                new Tuple<Operations, Literal, Literal, Literal>(Operations.BitOr, ValueFactory.Create(1), ValueFactory.Create(2), ValueFactory.Create(1 & 2)),
-                new Tuple<Operations, Literal, Literal, Literal>(Operations.BitXor, ValueFactory.Create(1), ValueFactory.Create(2), ValueFactory.Create(1 ^ 2)),
-                new Tuple<Operations, Literal, Literal, Literal>(Operations.Mod, ValueFactory.Create(5), ValueFactory.Create(2), ValueFactory.Create(1)),
-                new Tuple<Operations, Literal, Literal, Literal>(Operations.ShiftLeft, ValueFactory.Create(5), ValueFactory.Create(2), ValueFactory.Create(5 << 2)),
-                new Tuple<Operations, Literal, Literal, Literal>(Operations.ShiftRight, ValueFactory.Create(5), ValueFactory.Create(2), ValueFactory.Create(5 >> 2)),
+                new Tuple<Operations, Literal, Literal, Literal>(Operations.Add, LiteralFactory.Create(2), LiteralFactory.Create(3), LiteralFactory.Create(5)),
+                new Tuple<Operations, Literal, Literal, Literal>(Operations.Sub, LiteralFactory.Create(5), LiteralFactory.Create(3), LiteralFactory.Create(2)),
+                new Tuple<Operations, Literal, Literal, Literal>(Operations.Mul, LiteralFactory.Create(2), LiteralFactory.Create(3), LiteralFactory.Create(6)),
+                new Tuple<Operations, Literal, Literal, Literal>(Operations.Div, LiteralFactory.Create(6), LiteralFactory.Create(3), LiteralFactory.Create(2)),
+                new Tuple<Operations, Literal, Literal, Literal>(Operations.BitAnd, LiteralFactory.Create(1), LiteralFactory.Create(1), LiteralFactory.Create(1)),
+                new Tuple<Operations, Literal, Literal, Literal>(Operations.BitOr, LiteralFactory.Create(1), LiteralFactory.Create(2), LiteralFactory.Create(1 & 2)),
+                new Tuple<Operations, Literal, Literal, Literal>(Operations.BitXor, LiteralFactory.Create(1), LiteralFactory.Create(2), LiteralFactory.Create(1 ^ 2)),
+                new Tuple<Operations, Literal, Literal, Literal>(Operations.Mod, LiteralFactory.Create(5), LiteralFactory.Create(2), LiteralFactory.Create(1)),
+                new Tuple<Operations, Literal, Literal, Literal>(Operations.ShiftLeft, LiteralFactory.Create(5), LiteralFactory.Create(2), LiteralFactory.Create(5 << 2)),
+                new Tuple<Operations, Literal, Literal, Literal>(Operations.ShiftRight, LiteralFactory.Create(5), LiteralFactory.Create(2), LiteralFactory.Create(5 >> 2))
             };
 
             foreach (var test in tests)
@@ -141,6 +121,52 @@ namespace Weverca.TaintedAnalysis.UnitTest.FlowResolverTests
                         test.Item4))
                     .AddResult(ConditionForm.All, true, ConditionResults.True)
                     .AddResult(ConditionForm.None, false, ConditionResults.True)
+                    .Run();
+            }
+        }
+
+        [TestMethod]
+        public void NonequelityExpressions()
+        {
+            //BinaryEx.Operation, boudary (like a > boundary), positiveResult, negativeResult
+            Tuple<Operations, Literal, Value, Value>[] tests = new Tuple<Operations, Literal, Value, Value>[]
+            {
+                new Tuple<Operations, Literal, Value, Value>(Operations.GreaterThan, LiteralFactory.Create(2), new IntegerIntervalValue(3, int.MaxValue), new IntegerIntervalValue(int.MinValue, 2)),
+                new Tuple<Operations, Literal, Value, Value>(Operations.GreaterThanOrEqual, LiteralFactory.Create(2), new IntegerIntervalValue(2, int.MaxValue), new IntegerIntervalValue(int.MinValue, 1)),
+                new Tuple<Operations, Literal, Value, Value>(Operations.LessThan, LiteralFactory.Create(2), new IntegerIntervalValue(int.MinValue, 1), new IntegerIntervalValue(2, int.MaxValue)),
+                new Tuple<Operations, Literal, Value, Value>(Operations.LessThanOrEqual, LiteralFactory.Create(2), new IntegerIntervalValue(int.MinValue, 2), new IntegerIntervalValue(3, int.MaxValue)),
+
+                new Tuple<Operations, Literal, Value, Value>(Operations.GreaterThan, LiteralFactory.Create(2L), new LongintIntervalValue(3, long.MaxValue), new LongintIntervalValue(long.MinValue, 2)),
+                new Tuple<Operations, Literal, Value, Value>(Operations.GreaterThanOrEqual, LiteralFactory.Create(2L), new LongintIntervalValue(2, long.MaxValue), new LongintIntervalValue(long.MinValue, 1)),
+                new Tuple<Operations, Literal, Value, Value>(Operations.LessThan, LiteralFactory.Create(2L), new LongintIntervalValue(long.MinValue, 1), new LongintIntervalValue(2, long.MaxValue)),
+                new Tuple<Operations, Literal, Value, Value>(Operations.LessThanOrEqual, LiteralFactory.Create(2L), new LongintIntervalValue(long.MinValue, 2), new LongintIntervalValue(3, long.MaxValue)),
+                
+                new Tuple<Operations, Literal, Value, Value>(Operations.GreaterThan, LiteralFactory.Create(2.2), new FloatIntervalValue(2.2 + double.Epsilon, int.MaxValue), new FloatIntervalValue(double.MinValue, 2.2)),
+                new Tuple<Operations, Literal, Value, Value>(Operations.GreaterThanOrEqual, LiteralFactory.Create(2.2), new FloatIntervalValue(2.2, double.MaxValue), new FloatIntervalValue(double.MinValue, 2.2 - double.Epsilon)),
+                new Tuple<Operations, Literal, Value, Value>(Operations.LessThan, LiteralFactory.Create(2.2), new FloatIntervalValue(double.MinValue, 2.2 - double.Epsilon), new FloatIntervalValue(2.2, double.MaxValue)),
+                new Tuple<Operations, Literal, Value, Value>(Operations.LessThanOrEqual, LiteralFactory.Create(2.2), new FloatIntervalValue(double.MinValue, 2.2), new FloatIntervalValue(2.2 + double.Epsilon, double.MaxValue)),
+
+                new Tuple<Operations, Literal, Value, Value>(Operations.GreaterThan, LiteralFactory.Create("aaa"), new AnyStringValue(), new AnyStringValue()),
+                new Tuple<Operations, Literal, Value, Value>(Operations.GreaterThanOrEqual, LiteralFactory.Create("aaa"), new AnyStringValue(), new AnyStringValue()),
+                new Tuple<Operations, Literal, Value, Value>(Operations.LessThan, LiteralFactory.Create("aaa"), new AnyStringValue(), new AnyStringValue()),
+                new Tuple<Operations, Literal, Value, Value>(Operations.LessThanOrEqual, LiteralFactory.Create("aaa"), new AnyStringValue(), new AnyStringValue())
+            };
+
+            string variableName = "a";
+
+            foreach (var test in tests)
+            {
+                TestCase.Create(new BinaryEx(test.Item1, new DirectVarUse(new Position(), new VariableName(variableName)), test.Item2))
+                    .AddResult(ConditionForm.All, true, ConditionResults.True).AddResultValue(variableName, test.Item3)
+                    .AddResult(ConditionForm.None, false, ConditionResults.True)
+                    .AddResult(ConditionForm.None, true, ConditionResults.False).AddResultValue(variableName, test.Item4)
+                    .Run();
+
+                //flipped
+                TestCase.Create(new BinaryEx(test.Item1, test.Item2, new DirectVarUse(new Position(), new VariableName(variableName))))
+                    .AddResult(ConditionForm.All, true, ConditionResults.True).AddResultValue(variableName, test.Item4)
+                    .AddResult(ConditionForm.None, false, ConditionResults.True)
+                    .AddResult(ConditionForm.None, true, ConditionResults.False).AddResultValue(variableName, test.Item3)
                     .Run();
             }
         }
--- VSProjects/Realisation/Weverca.TaintedAnalysis.UnitTest/FlowResolverTests/ValueFactory.cs
+++ VSProjects/Realisation/Weverca.TaintedAnalysis.UnitTest/FlowResolverTests/ValueFactory.cs
@@ -1,13 +1,22 @@
-﻿using PHP.Core.AST;
+﻿using System;
+
+using PHP.Core.AST;
 using PHP.Core.Parsers;
+
+using Weverca.Analysis.Memory;
 
 namespace Weverca.TaintedAnalysis.UnitTest.FlowResolverTests
 {
-    static class ValueFactory
+    static class LiteralFactory
     {
         public static IntLiteral Create(int value)
         {
             return new IntLiteral(new Position(), value);
+        }
+
+        public static LongIntLiteral Create(long value)
+        {
+            return new LongIntLiteral(new Position(), value);
         }
 
         public static DoubleLiteral Create(double value)
@@ -23,6 +32,34 @@ namespace Weverca.TaintedAnalysis.UnitTest.FlowResolverTests
         public static BoolLiteral Create(bool value)
         {
             return new BoolLiteral(new Position(), value);
+        }
+    }
+
+    static class LiteralValueFactory
+    {
+        public static Tuple<Literal, Value> Create(int value)
+        {
+            return new Tuple<Literal, Value>(LiteralFactory.Create(value), new IntegerValue(value));
+        }
+
+        public static Tuple<Literal, Value> Create(long value)
+        {
+            return new Tuple<Literal, Value>(LiteralFactory.Create(value), new LongintValue(value));
+        }
+
+        public static Tuple<Literal, Value> Create(double value)
+        {
+            return new Tuple<Literal, Value>(LiteralFactory.Create(value), new FloatValue(value));
+        }
+
+        public static Tuple<Literal, Value> Create(string value)
+        {
+            return new Tuple<Literal, Value>(LiteralFactory.Create(value), new StringValue(value));
+        }
+
+        public static Tuple<Literal, Value> Create(bool value)
+        {
+            return new Tuple<Literal, Value>(LiteralFactory.Create(value), new BooleanValue(value));
         }
     }
 }
