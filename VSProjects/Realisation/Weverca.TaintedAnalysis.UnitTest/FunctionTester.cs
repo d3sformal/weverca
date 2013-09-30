2d8e4753907906ab6cf0bc2d8dc2acb0ea358511
kikta <kikta@mycompany.com>
2013-09-30 21:12:08 +0000

    zabudnuty subor s testami
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@337 67bba403-d36b-46c0-a350-761a85a4c57d

---
+++ VSProjects/Realisation/Weverca.TaintedAnalysis.UnitTest/FunctionTester.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Weverca.Analysis.Memory;
+
+namespace Weverca.TaintedAnalysis.UnitTest
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class FunctionTester
+    {
+
+        string FunctionBeforeDeclarationCallTest= @"
+            $result=a(0);
+            function a($x)
+            {
+            return 4;
+            }
+        ";
+
+        string MethodBeforeDeclarationCallTest = @"
+            $x = new A();
+            $result = $x->a(4);
+            class A
+            {
+                function a($x)
+                {
+                return 4;
+                }
+            }
+        ";
+
+
+        [TestMethod]
+        public void FunctionBeforeDeclarationCall()
+        {
+            var result = TestUtils.ResultTest(FunctionBeforeDeclarationCallTest);
+            TestUtils.testType(result, typeof(IntegerValue));
+            TestUtils.testValue(result,4);
+        }
+
+        [TestMethod]
+        public void MethodBeforeDeclarationCall()
+        {
+            var result = TestUtils.ResultTest(MethodBeforeDeclarationCallTest);
+            TestUtils.testType(result, typeof(IntegerValue));
+            TestUtils.testValue(result, 4);
+        }
+
+    }
+}
