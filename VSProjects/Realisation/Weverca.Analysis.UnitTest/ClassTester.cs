d16e82c2e2a86d413633670d038158a1ad3160ab
kikta <kikta@mycompany.com>
2013-10-10 12:04:39 +0000

    WEV-42 zabudnuty subor s testami
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@369 67bba403-d36b-46c0-a350-761a85a4c57d

---
+++ VSProjects/Realisation/Weverca.Analysis.UnitTest/ClassTester.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PHP.Core;
+using Weverca.AnalysisFramework.Memory;
+
+namespace Weverca.Analysis.UnitTest
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class ClassTester
+    {
+        string ClassExtendTest = @"
+           class A
+           {
+               public $x=4;
+           }
+           class B extends A
+           {
+               public $y=4;
+           }
+        ";
+
+        [TestMethod]
+        public void ClassExtend()
+        {
+            var outset = TestUtils.Analyze(ClassExtendTest);
+            var it = (outset.ResolveType(new QualifiedName(new Name("B"))).GetEnumerator());
+            it.MoveNext();
+            TypeValue B=it.Current as TypeValue;
+            Debug.Equals(B.Declaration.Fields.Count,2);
+            Debug.Assert(B.Declaration.Fields[new VariableName("x")] != null);
+            Debug.Assert(B.Declaration.Fields[new VariableName("y")] != null);
+
+            it = (outset.ResolveType(new QualifiedName(new Name("A"))).GetEnumerator());
+            it.MoveNext();
+            TypeValue A = it.Current as TypeValue;
+            Debug.Equals(A.Declaration.Fields.Count, 1);
+            Debug.Assert(A.Declaration.Fields[new VariableName("x")]!=null);
+
+        }
+    }
+}
