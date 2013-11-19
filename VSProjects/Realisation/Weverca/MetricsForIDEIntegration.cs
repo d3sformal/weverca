4798caec4a015be0272ff5ea8ef612cb2117a234
hauzar <hauzar@mycompany.com>
2013-11-19 11:39:35 +0000

    Printing information used for eclipse integration plugin (code metrics visualisation).
    Do not modify the format of the output - eclipse plugin depends on this format. For printing the information about code metrics in a better way, use another option of Weverca executable.
    
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@422 67bba403-d36b-46c0-a350-761a85a4c57d

---
+++ VSProjects/Realisation/Weverca/MetricsForIDEIntegration.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Weverca.Parsers;
+using Weverca.CodeMetrics;
+using PHP.Core;
+using PHP.Core.AST;
+
+namespace Weverca
+{
+    /// <summary>
+    /// Computation of code metrics for IDE integration
+    /// </summary>
+    class MetricsForIDEIntegration
+    {
+        public static readonly string PHP_FILE_EXTENSION = ".php";
+
+        /// <summary>
+        /// Runs computation of code metrics for IDE integration (option -cmide of the analyzer)
+        /// Do not modify the format of the output. For another format of output use another option of the analyzer.
+        /// </summary>
+        internal static void Run(string metricsType, string analyzedFile, string[] metricsArgs)
+        {
+            //The first argument determines an action
+            if (metricsType == "-quantity")
+            {
+                if (Directory.Exists(analyzedFile))
+                {
+                    ProcessDirectory(analyzedFile);
+                }
+                else if (File.Exists(analyzedFile))
+                {
+                    string fileExtension = Path.GetExtension(analyzedFile);
+                    if (fileExtension == PHP_FILE_EXTENSION)
+                    {
+                        ProcessFile(analyzedFile);
+                    }
+                }
+            }
+            else if (metricsType == "-constructs" && metricsArgs.Length > 0)
+            {
+                if (Directory.Exists(analyzedFile))
+                {
+                    ProcessDirectory(analyzedFile, metricsArgs);
+                }
+                else if (File.Exists(analyzedFile))
+                {
+                    string fileExtension = Path.GetExtension(analyzedFile);
+                    if (fileExtension == PHP_FILE_EXTENSION)
+                    {
+                        ProcessFile(analyzedFile, metricsArgs);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// For each PHP sources in the given directory calls ProcessFile method.
+        /// </summary>
+        /// <param name="directoryName">Name of the directory.</param>
+        private static void ProcessDirectory(string directoryName)
+        {
+            Console.WriteLine("Processing directory: {0}", directoryName);
+            System.Diagnostics.Debug.Assert(Directory.Exists(directoryName));
+
+            foreach (string fileName in Directory.EnumerateFiles(directoryName, "*.php", SearchOption.TopDirectoryOnly))
+            {
+                string fileExtension = Path.GetExtension(fileName);
+                if (fileExtension == PHP_FILE_EXTENSION)
+                {
+                    ProcessFile(fileName);
+                }
+            }
+        }
+        private static void ProcessDirectory(string directoryName, string[] constructs)
+        {
+            Console.WriteLine("Processing directory: {0}", directoryName);
+            System.Diagnostics.Debug.Assert(Directory.Exists(directoryName));
+
+            foreach (string fileName in Directory.EnumerateFiles(directoryName, "*.php", SearchOption.AllDirectories))
+            {
+
+                string fileExtension = Path.GetExtension(fileName);
+                if (fileExtension == PHP_FILE_EXTENSION)
+                {
+                    ProcessFile(fileName, constructs);
+                }
+            }
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="fileName">Name of the file.</param>
+        private static void ProcessFile(string fileName)
+        {
+            Console.WriteLine("Process file: {0}", fileName);
+
+            System.Diagnostics.Debug.Assert(File.Exists(fileName));
+
+            Console.WriteLine("Processing file: {0}", fileName);
+            SyntaxParser parser = GenerateParser(fileName);
+            parser.Parse();
+            if (parser.Errors.AnyError) return;
+            MetricInfo info = MetricInfo.FromParsers(true, parser);
+            Console.WriteLine(fileName + "," + info.GetQuantity(Quantity.NumberOfLines) + "," + info.GetQuantity(Quantity.NumberOfSources) + "," +
+            info.GetQuantity(Quantity.MaxInheritanceDepth) + "," + info.GetQuantity(Quantity.MaxMethodOverridingDepth) + "," +
+                //info.GetRating(Rating.Cyclomacity + "," +  NOT IMPLEMENTED
+            info.GetRating(Rating.ClassCoupling) + "," + info.GetRating(Rating.PhpFunctionsCoupling));
+        }
+
+        private static void ProcessFile(string fileName, string[] constructs)
+        {
+            Console.WriteLine("Process file: {0}", fileName);
+
+            System.Diagnostics.Debug.Assert(File.Exists(fileName));
+
+            Console.WriteLine("Processing file: {0}", fileName);
+            SyntaxParser parser = GenerateParser(fileName);
+            parser.Parse();
+            if (parser.Errors.AnyError)
+            {
+                Console.WriteLine("error");
+                return;
+            }
+            MetricInfo info = MetricInfo.FromParsers(true, parser);
+
+            foreach (string construct in constructs)
+            {
+                if (info.HasIndicator(StringToIndicator(construct)))
+                    foreach (AstNode node in info.GetOccurances(StringToIndicator(construct)))
+                    {
+                        Console.WriteLine("File *" + fileName + "* contains indicator *" + construct + "*" + ((LangElement)node).Position.FirstLine.ToString() + "," + ((LangElement)node).Position.FirstOffset.ToString() +
+                                          "," + ((LangElement)node).Position.LastLine.ToString() + "," + ((LangElement)node).Position.LastOffset.ToString());
+                    }
+            }
+
+
+
+
+        }
+
+        private static ConstructIndicator StringToIndicator(string construct)
+        {
+            // TODO pouzit Dictionary - nie je kompletny
+
+            if (construct == "Aliasing")
+            { return ConstructIndicator.Alias; }
+            if (construct == "Autoload")
+            { return ConstructIndicator.Autoload; }
+            if (construct == "Class presence")
+            { return ConstructIndicator.Class; }
+            if (construct == "Dynamic call")
+            { return ConstructIndicator.DynamicCall; }
+            if (construct == "Dynamic dereference")
+            { return ConstructIndicator.DynamicDereference; }
+            if (construct == "Dynamic include")
+            { return ConstructIndicator.DynamicInclude; }
+            if (construct == "Eval")
+            { return ConstructIndicator.Eval; }
+            if (construct == "Inside function declaration")
+            { return ConstructIndicator.InsideFunctionDeclaration; }
+            if (construct == "Magic methods")
+            { return ConstructIndicator.MagicMethod; }
+            if (construct == "SQL")
+            { return ConstructIndicator.MySQL; }
+            if (construct == "Passing by reference at call side")
+            { return ConstructIndicator.PassingByReferenceAtCallSide; }
+            if (construct == "Sessions")
+            { return ConstructIndicator.Session; }
+            else //(construct == "Use of super global variable")
+            { return ConstructIndicator.SuperGlobalVariable; }
+            // return null; - non-nullable         
+        }
+
+
+
+        private static SyntaxParser GenerateParser(string fileName)
+        {
+            string code;
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                code = reader.ReadToEnd();
+            }
+
+            PhpSourceFile source_file = new PhpSourceFile(new FullPath(Path.GetDirectoryName(fileName)), new FullPath(fileName));
+            return new SyntaxParser(source_file, code);
+        }
+    }
+}
--- VSProjects/Realisation/Weverca/Program.cs
+++ VSProjects/Realisation/Weverca/Program.cs
@@ -27,17 +27,41 @@ namespace Weverca
         /// <param name="args">TODO: Specification of arguments</param>
         static void Main(string[] args)
         {
-            if (args.Count() < 1)
+            if (args.Count() < 2)
             {
                 Console.WriteLine("Missing argument");
-                Console.WriteLine(@"Example of usage: weverca.exe ..\..\..\..\..\PHP_sources\test_programs\testfile.php ");
+                Console.WriteLine(@"Example of usage: weverca.exe -options ..\..\..\..\..\PHP_sources\test_programs\testfile.php ");
+                Console.WriteLine(@"-sa");
+                Console.WriteLine(@"  Static analysis");
+                Console.WriteLine(@"-cmide [-options_cmide]");
+                Console.WriteLine(@"  Code metrics for IDE integration");
+                Console.WriteLine(@"  -cmide -constructs list_of_constructs_separated_by_space");
+                Console.WriteLine(@"    Constructs search");
+                Console.WriteLine(@"  -cmide -quantity");
+                Console.WriteLine(@"    Quantity and rating code metrics computation");
                 Console.ReadKey();
                 return;
             }
             
             //TODO: Resolve entry file
-            var analyzedFile = args[0];
+            var analysisType = args[0];
+            var analyzedFile = args[args.Length-1];
 
+            switch (analysisType)
+            {
+                case "-sa":
+                    RunStaticAnalysis(analyzedFile);
+                    break;
+                case "-cmide":
+                    string[] metricsArgs = new string[args.Length-3];
+                    Array.Copy(args, 2, metricsArgs, 0, args.Length - 3);
+                    MetricsForIDEIntegration.Run(args[1], analyzedFile, metricsArgs);
+                    break;
+            }
+        }
+
+        static private void RunStaticAnalysis(string analyzedFile) 
+        {
             //TODO: this is for time consumption analyzing only
             //Analyze twice - because of omitting .NET initialization we get better analysis time
             Analyzer.Run(analyzedFile);
@@ -50,7 +74,7 @@ namespace Weverca
             //Build output
             var console = new ConsoleOutput();
             var graphWalker = new GraphWalking.CallGraphPrinter(ppGraph);
-            console.CommentLine(string.Format("Analysis completed in: {0}ms\n",watch.ElapsedMilliseconds));
+            console.CommentLine(string.Format("Analysis completed in: {0}ms\n", watch.ElapsedMilliseconds));
 
             graphWalker.Run(console);
             console.CommentLine(string.Format("Analysis completed in: {0}ms\n", watch.ElapsedMilliseconds));
