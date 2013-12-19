ad7e1f69166588f8ca5d711f242066fc45e4caef
brenner <brenner@mycompany.com>
2013-12-19 13:31:46 +0000

    WEV-45 Založení projektu webového GUI
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@467 67bba403-d36b-46c0-a350-761a85a4c57d

---
+++ VSProjects/Realisation/Weverca.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Common.WebDefinitions.Debug;
+
+namespace Weverca.Web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorLogAttribute());
+        }
+    }
+}
