8128282ead92511088774563ec20eb51c509a563
kikta <kikta@mycompany.com>
2013-12-23 20:35:25 +0000

    zabudnuty subor
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@476 67bba403-d36b-46c0-a350-761a85a4c57d

---
+++ VSProjects/Realisation/Weverca.Analysis.UnitTest/FlagTester.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PHP.Core;
+using Weverca.AnalysisFramework.Memory;
+using Weverca.AnalysisFramework;
+
+namespace Weverca.Analysis.UnitTest
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class FlagTester
+    {
+
+        string SimpleFlagTest = @"
+          $result=$_POST['x'];
+        ";
+
+        [TestMethod]
+        public void SimpleFlag()
+        {
+            var result = TestUtils.ResultTest(SimpleFlagTest);
+            TestUtils.IsDirty(result);
+           
+        }
+
+        string SimpleFlagTest2 = @"
+          $result=substr ($_POST['x'],0);
+        ";
+
+        [TestMethod]
+        public void SimpleFlag2()
+        {
+            var result = TestUtils.ResultTest(SimpleFlagTest2);
+            TestUtils.IsDirty(result);
+
+        }
+
+        string SimpleFlagTest3 = @"
+          $result=strlen ($_POST['x']);
+        ";
+
+        [TestMethod]
+        public void SimpleFlag3()
+        {
+            var result = TestUtils.ResultTest(SimpleFlagTest3);
+            TestUtils.IsClean(result);
+
+        }
+
+        string SimpleFlagTest4 = @"
+          $result= $_POST['x'].'a';
+        ";
+
+        [TestMethod]
+        public void SimpleFlag4()
+        {
+            var result = TestUtils.ResultTest(SimpleFlagTest4);
+            TestUtils.IsDirty(result);
+
+        }
+
+    }
+}
