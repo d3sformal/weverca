23c969d4526a991277d3ee68967c7449986a259a
kikta <kikta@mycompany.com>
2014-01-18 15:54:09 +0000

    zabudnuty subor s testom
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@528 67bba403-d36b-46c0-a350-761a85a4c57d

---
+++ VSProjects/Realisation/Weverca.Analysis.UnitTest/ExceptionTester.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace Weverca.Analysis.UnitTest
+{
+    /// <summary>
+    /// Summary description for ExceptionTester
+    /// </summary>
+    [TestClass]
+    public class ExceptionTester
+    {
+
+        string ThrownStringTest = @"
+           try
+        {
+            $x='x';
+            throw $x;
+        }
+        catch(Exception $e)
+        {
+
+        }
+        ";
+
+        [TestMethod]
+        public void ThrownString()
+        {
+            var outset = TestUtils.Analyze(ThrownStringTest);
+            Debug.Assert(TestUtils.ContainsWarning(outset, AnalysisWarningCause.ONLY_OBJECT_CAM_BE_THROWN));
+        }
+
+    }
+}
