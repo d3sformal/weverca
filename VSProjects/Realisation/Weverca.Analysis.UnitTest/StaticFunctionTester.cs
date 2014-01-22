9e85fd106ddc277f9e55ac4db989bbfd19277c57
kikta <kikta@mycompany.com>
2014-01-22 22:52:03 +0000

    pridane static call testy zatial failuju
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@539 67bba403-d36b-46c0-a350-761a85a4c57d

---
+++ VSProjects/Realisation/Weverca.Analysis.UnitTest/StaticFunctionTester.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PHP.Core;
+using Weverca.AnalysisFramework.Memory;
+using Weverca.AnalysisFramework;
+
+namespace Weverca.Analysis.UnitTest
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class StaticFunctionTester
+    {
+
+        string StaticCallTest = @"
+         $result=0;
+        class a
+        {
+            static function b()
+            {
+                global $result;
+                $result=4;
+            }
+        }
+        a::b();
+
+        ";
+
+        [TestMethod]
+        public void StaticCall()
+        {
+            var result = TestUtils.ResultTest(StaticCallTest);
+            TestUtils.testType(result, typeof(IntegerValue));
+            TestUtils.testValue(result, 4);
+
+        }
+
+
+        string StaticCallTest2 = @"
+         $result=0;
+        class a
+        {
+            static function b()
+            {
+                global $result;
+                $result=4;
+            }
+        }
+        $a=new a();
+        $a::b();
+
+        ";
+
+        [TestMethod]
+        public void StaticCall2()
+        {
+            var result = TestUtils.ResultTest(StaticCallTest2);
+            TestUtils.testType(result, typeof(IntegerValue));
+            TestUtils.testValue(result, 4);
+
+        }
+
+    }
+}
