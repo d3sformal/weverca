9f215d64095780b22ecdd4b0f3b436faa5927e17
kikta <kikta@mycompany.com>
2014-02-11 20:09:40 +0000

    zabudnuty test file
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@654 67bba403-d36b-46c0-a350-761a85a4c57d

---
+++ VSProjects/Realisation/Weverca.Analysis.UnitTest/IndexTester.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PHP.Core;
+using Weverca.AnalysisFramework.Memory;
+using Weverca.AnalysisFramework;
+
+namespace Weverca.Analysis.UnitTest
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class IndexTester
+    {
+        string IndexIntergerTest = @"
+            $a=4;
+            $p=$a[""a""];    
+        ";
+
+
+        [TestMethod]
+        public void IndexInterger()
+        {
+            var result = TestUtils.Analyze(IndexIntergerTest);
+            Debug.Assert(TestUtils.ContainsWarning(result, AnalysisWarningCause.CANNOT_ACCESS_FIELD_OPERATOR_ON_NON_ARRAY));
+
+        }
+
+        string IndexIntergerTest2 = @"
+            $a=4;
+            $a[""a""]=15;    
+        ";
+
+
+        [TestMethod]
+        public void IndexInterger2()
+        {
+            var result = TestUtils.Analyze(IndexIntergerTest2);
+            Debug.Assert(TestUtils.ContainsWarning(result, AnalysisWarningCause.CANNOT_ACCESS_FIELD_OPERATOR_ON_NON_ARRAY));
+
+        }
+
+
+    }
+}
