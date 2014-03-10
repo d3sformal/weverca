112a91a17f195a34546e72e7f66ccd885810cdc6
skorvaga <skorvaga@mycompany.com>
2014-03-10 12:50:28 +0000

    Added htmlentities and md5 simple implementations
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@830 67bba403-d36b-46c0-a350-761a85a4c57d

--- VSProjects/Realisation/Weverca.Analysis/NativeAnalyzers/NativeFunctionsConcreteImplementations.cs
+++ VSProjects/Realisation/Weverca.Analysis/NativeAnalyzers/NativeFunctionsConcreteImplementations.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
 
 using PHP.Core;
+using PHP.Library;
 
+using Weverca.Analysis.ExpressionEvaluator;
 using Weverca.AnalysisFramework;
 using Weverca.AnalysisFramework.Memory;
-using Weverca.Analysis.ExpressionEvaluator;
 
 namespace Weverca.Analysis.NativeAnalyzers
 {
@@ -17,6 +18,8 @@ namespace Weverca.Analysis.NativeAnalyzers
     {
         #region Utility methods and fields
 
+        private static readonly BooleanConverter booleanConverter = new BooleanConverter();
+
         private static readonly StringConverter stringConverter = new StringConverter();
 
         /// <summary>
@@ -24,44 +27,69 @@ namespace Weverca.Analysis.NativeAnalyzers
         /// 
         /// Note that all functions that have concrete implementation have to be also type-modeled.
         /// </summary>
-        /// <param name="typeModeledFunctions">the dictionary of type-modeled native functions</param>
-        /// <param name="concreteFunctions">(output parameter) the dictionary of concrete native functions to which functions implemented in this class will be added</param>
-        public static void AddConcreteFunctions(Dictionary<QualifiedName, List<NativeFunction>> typeModeledFunctions, Dictionary<QualifiedName, NativeAnalyzerMethod> concreteFunctions)
+        /// <param name="typeModeledFunctions">The dictionary of type-modeled native functions.</param>
+        /// <param name="concreteFunctions">
+        /// (output parameter) the dictionary of concrete native functions
+        /// to which functions implemented in this class will be added.
+        /// </param>
+        public static void AddConcreteFunctions(
+            Dictionary<QualifiedName, List<NativeFunction>> typeModeledFunctions,
+            Dictionary<QualifiedName, NativeAnalyzerMethod> concreteFunctions)
         {
             foreach (var methodName in functions.Keys)
             {
                 var qualifiedName = new QualifiedName(new Name(methodName));
-                concreteFunctions.Add(qualifiedName, new NativeAnalyzerMethod(new ConcreteFunctionAnalyzerHelper(typeModeledFunctions[qualifiedName], functions[methodName]).analyze));
+                var analyzer = new ConcreteFunctionAnalyzerHelper(typeModeledFunctions[qualifiedName],
+                    functions[methodName]);
+                var method = new NativeAnalyzerMethod(analyzer.analyze);
+                concreteFunctions.Add(qualifiedName, method);
             }
         }
-        #endregion
+
+        #endregion Utility methods and fields
 
         /// <summary>
         /// Contains all pairs "name of the function - implementation of the function" that can be used by the analysis.
         /// </summary>
-        private static Dictionary<string, ConcreteFunctionDelegate> functions = new Dictionary<string, ConcreteFunctionDelegate>() 
+        private static Dictionary<string, ConcreteFunctionDelegate> functions
+            = new Dictionary<string, ConcreteFunctionDelegate>()
         {
             { "strtolower", _strtolower },
-            { "strtoupper", _strtoupper }
+            { "strtoupper", _strtoupper },
+            { "htmlentities", _htmlentities },
+            { "md5", _md5 }
         };
 
         #region Implementations of concrete native functions
+
         private static Value _strtolower(FlowController flow, Value[] arguments)
         {
+            Debug.Assert(arguments.Length == 1);
+
             stringConverter.SetContext(flow);
-            return flow.OutSet.CreateString(
-                PHP.Library.PhpStrings.ToLower(
-                    stringConverter.EvaluateToString(arguments[0]).Value)
-                );
+            var stringValue = stringConverter.EvaluateToString(arguments[0]);
+
+            if (stringValue == null)
+            {
+                return flow.OutSet.AnyStringValue;
+            }
+
+            return flow.OutSet.CreateString(PhpStrings.ToLower(stringValue.Value));
         }
 
         private static Value _strtoupper(FlowController flow, Value[] arguments)
         {
+            Debug.Assert(arguments.Length == 1);
+
             stringConverter.SetContext(flow);
-            return flow.OutSet.CreateString(
-                PHP.Library.PhpStrings.ToUpper(
-                    stringConverter.EvaluateToString(arguments[0]).Value)
-                );
+            var stringValue = stringConverter.EvaluateToString(arguments[0]);
+
+            if (stringValue == null)
+            {
+                return flow.OutSet.AnyStringValue;
+            }
+
+            return flow.OutSet.CreateString(PhpStrings.ToUpper(stringValue.Value));
         }
 
         private static Value _concat(FlowController flow, Value[] arguments)
@@ -69,7 +97,57 @@ namespace Weverca.Analysis.NativeAnalyzers
             stringConverter.SetContext(flow);
             return stringConverter.EvaluateConcatenation(arguments[0], arguments[1]);
         }
-        #endregion
 
+        private static Value _htmlentities(FlowController flow, Value[] arguments)
+        {
+            Debug.Assert(arguments.Length > 0);
+
+            if (arguments.Length > 1)
+            {
+                // TODO: Implement precisely
+                return flow.OutSet.AnyStringValue;
+            }
+
+            stringConverter.SetContext(flow);
+            var stringValue = stringConverter.EvaluateToString(arguments[0]);
+
+            if (stringValue == null)
+            {
+                return flow.OutSet.AnyStringValue;
+            }
+
+            return flow.OutSet.CreateString(PhpStrings.EncodeHtmlEntities(stringValue.Value));
+        }
+
+        private static Value _md5(FlowController flow, Value[] arguments)
+        {
+            stringConverter.SetContext(flow);
+            var stringValue = stringConverter.EvaluateToString(arguments[0]);
+
+            if (stringValue == null)
+            {
+                return flow.OutSet.AnyStringValue;
+            }
+
+            var phpBytes = new PhpBytes(stringValue.Value);
+
+            Debug.Assert(arguments.Length > 0);
+
+            if (arguments.Length > 1)
+            {
+                booleanConverter.SetContext(flow.OutSet.Snapshot);
+                var isRawOutput = booleanConverter.EvaluateToBoolean(arguments[1]);
+
+                if ((isRawOutput == null) || isRawOutput.Value)
+                {
+                    // TODO: Implement precisely
+                    return flow.OutSet.AnyStringValue;
+                }
+            }
+
+            return flow.OutSet.CreateString(PhpHash.MD5(phpBytes));
+        }
+
+        #endregion Implementations of concrete native functions
     }
 }
