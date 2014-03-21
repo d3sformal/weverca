225b940ab704103a357ace8a3dad487609facc57
hauzar <hauzar@mycompany.com>
2014-03-21 07:04:05 +0000

    Missing file.
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@856 67bba403-d36b-46c0-a350-761a85a4c57d

---
+++ VSProjects/Realisation/Weverca.AnalysisFramework/CallContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PHP.Core.Parsers;
+using Weverca.AnalysisFramework.ProgramPoints;
+
+namespace Weverca.AnalysisFramework
+{
+    /// <summary>
+    /// Represents a context of a program point graph.
+    /// Contains program points from which the graph is used (called or included).
+    /// </summary>
+    public class PPGraphContext
+    {
+        internal readonly List<ProgramPointBase> _callers = new List<ProgramPointBase>();
+
+        /// <summary>
+        /// Set of program points from which the owning program point graph is used (called).
+        /// </summary>
+        public IEnumerable<ProgramPointBase> Callers { get { return _callers; } }
+        /// <summary>
+        /// The program point graph which context is represented.
+        /// </summary>
+        public readonly ProgramPointGraph OwningPPGraph;
+
+        internal PPGraphContext(ProgramPointGraph owningPPGraph) { OwningPPGraph = owningPPGraph; }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+
+            //result.AppendLine(OwningPPGraph.OwningScript.FullName);
+            result.Append("->");
+            foreach (var caller in _callers)
+            {
+                // If the program point calls itself, continue (this occurs, e.g., in case sharing graphs in recursive calls)
+                if (OwningPPGraph == caller.OwningPPGraph)
+                    continue;
+
+                result.Append("(");
+
+                result.Append(caller.OwningPPGraph.OwningScript.FullName + " at " + caller.Partial.Position);
+                result.Append(caller.OwningPPGraph.Context);
+
+                result.Append(")");
+                result.AppendLine("or");
+
+                //result.Append("( " + caller.ToString() + " )");
+            }
+            //result.AppendLine();
+
+            return result.ToString();
+        }
+
+    }
+}
