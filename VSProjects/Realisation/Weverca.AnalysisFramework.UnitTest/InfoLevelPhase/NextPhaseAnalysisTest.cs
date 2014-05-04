3ae3027036f0e7f056cb3d9dbd750581754ec11f
bastecky <bastecky@mycompany.com>
2014-05-04 10:40:57 +0000

    Copy memory model * Added test for bugfixing
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@925 67bba403-d36b-46c0-a350-761a85a4c57d

--- VSProjects/Realisation/Weverca.AnalysisFramework.UnitTest/InfoLevelPhase/NextPhaseAnalysisTest.cs
+++ VSProjects/Realisation/Weverca.AnalysisFramework.UnitTest/InfoLevelPhase/NextPhaseAnalysisTest.cs
@@ -218,7 +218,49 @@ $str[0] = $x;
 $alias = &$a[$_POST[1]];
 ".AssertVariable("").Analysis(Analyses.WevercaAnalysisTest);
 
+        readonly static TestCase IntoMethod_CASE = @"
+class human {
+public $gender;
+
+public function __construct($gender)
+{
+$this->gender = $gender;
+
+echo self::get_gender();
+}
+
+public function get_gender()
+{
+return $this->gender;
+}
+}
+
+class person extends human {
+public $name;
+
+public function set_name($name)
+{
+$this->name = $name;
+}
+}
+
+$Johnny = new person('male');
+echo $Johnny->get_gender();
+
+$Mary = new person('female');
+$Mary->set_name('Mary');
+
+".AssertVariable("Johnny").HasTaintStatus(false)
+            .Analysis(Analyses.WevercaAnalysisTest);
+
+
         #endregion Taint analysis
+
+        [TestMethod]
+        public void IntoMethod()
+        {
+            AnalysisTestUtils.RunInfoLevelTaintAnalysisCase(IntoMethod_CASE);
+        }
 
         [TestMethod]
         public void MayAliasTest()
--- VSProjects/Realisation/Weverca.MemoryModels/CopyMemoryModel/Snapshot/SnapshotLogger.cs
+++ VSProjects/Realisation/Weverca.MemoryModels/CopyMemoryModel/Snapshot/SnapshotLogger.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#define COPY_SNAPSHOT_LOG
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
