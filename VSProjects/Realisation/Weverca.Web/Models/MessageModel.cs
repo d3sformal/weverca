04f5cdbbc5b4023e7e89506ac3c61a362677a152
hauzar <hauzar@mycompany.com>
2014-08-29 09:07:30 +0000

    Added LGPL licence.
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@974 67bba403-d36b-46c0-a350-761a85a4c57d

--- VSProjects/Realisation/Weverca.Web/Models/MessageModel.cs
+++ VSProjects/Realisation/Weverca.Web/Models/MessageModel.cs
@@ -1,7 +1,24 @@
-﻿
-namespace Weverca.Web.Models
-{
-    public class MessageModel
+/*
+Copyright (c) 2012-2014 Matyas Brenner and David Hauzar
+
+This file is part of WeVerca.
+
+WeVerca is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or 
+(at your option) any later version.
+
+WeVerca is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with WeVerca.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+ublic class MessageModel
     {
         public string Title { get; private set; }
         public string Message { get; private set; }
