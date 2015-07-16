120471a7194a1a66ada358d9d3fa78bb3649c53f
hauzar <hauzar@mycompany.com>
2015-07-16 16:11:32 +0000

    Adding docuwiki to test applications.
    git-svn-id: svn+ssh://aiya.ms.mff.cuni.cz/var/svn/weverca/trunk@1102 67bba403-d36b-46c0-a350-761a85a4c57d

---
+++ PHP_sources/test_programs/dokuwiki_2014-09-29a/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+david:$1$e8ErD6p2$7XmMhSgEijJSNcN2zw8RX1:David Hauzar:david.hauzar@gmail.com:admin,user
